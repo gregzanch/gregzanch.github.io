7313b4f1f40dc3bf82c42936fc715e89260114eb
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-04-25 18:47:35 -0400

    first build

---
+++ src/projects/cram/index.tsx
@@ -0,0 +1,15 @@
+
+
+import cram1 from './thumbs/cram1.png';
+import cram2 from './thumbs/cram2.png';
+import cram3 from './thumbs/cram3.png';
+
+const thumbnails = [cram1, cram2, cram3];
+const title = "cram.ui"
+const description = "Computational Room Acoustics Module"
+
+export {
+  thumbnails,
+  description,
+  title
+}
