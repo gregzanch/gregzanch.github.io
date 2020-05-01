2e08612f492b86acbae934ce1df60adc8c384573
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-05-01 15:37:53 -0400

    Updates

---
+++ precache-manifest.6ec25bf80f33320902616a56b5db4d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1779a1a4a9f33a420f8fbd77fd202e",
+    "url": "/gregzanch.github.io/index.html"
+  },
+  {
+    "revision": "5afad83fe27adf12d342",
+    "url": "/gregzanch.github.io/static/css/main.b04805a5.chunk.css"
+  },
+  {
+    "revision": "f74bbfbec052807b655e",
+    "url": "/gregzanch.github.io/static/js/2.e19c0eeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gregzanch.github.io/static/js/2.e19c0eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afad83fe27adf12d342",
+    "url": "/gregzanch.github.io/static/js/main.0fa2534b.chunk.js"
+  },
+  {
+    "revision": "6af940a7b19f3bf0a89e",
+    "url": "/gregzanch.github.io/static/js/runtime-main.a27637a8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18de13f0a48d6b36abf50af77428850a.js"
+  "/gregzanch.github.io/precache-manifest.6ec25bf80f33320902616a56b5db4d84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gregzanch.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
